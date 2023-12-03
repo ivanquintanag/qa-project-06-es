57ec35e5103d92a35df904460d8b80ffde0a7948
ivanquintanag <152566024+ivanquintanag@users.noreply.github.com>
2023-12-03 19:57:00 -0300

    configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,4 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
+URL_SERVICE = "https://e93f80db-7ed1-4cb2-b3cb-0e86dde344db.serverhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+CREATE_USER_PATH = "/api/v1/users" # Almacena la ruta para crear un usuario o usuaria en esta variable
+CREATE_KIT = "/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
+DOC_PATH = '/docs/'
